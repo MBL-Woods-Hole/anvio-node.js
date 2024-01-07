638e51c24c72d727860c7d99aeada8ed49bc0a04
avoorhis <avoorhis@mbl.edu>
2024-01-07 08:43:59 -0500

    test

--- public/scripts/anvio_port_monitor.py
+++ public/scripts/anvio_port_monitor.py
@@ -123,7 +123,7 @@ def check_port_monitor_log_size():
     #print(port_monitor_log,'filesize',filesize)
     current_size = args.logfilep.tell()
     #print(port_monitor_log,'f.tellcurrent_size',current_size)
-    if current_size > 500000:  # 6,000,000
+    if current_size > 6000000:  # 6,000,000
         #args.logfilep.close()
         #args.logfilep = open(port_monitor_log, 'a')
         args.logfilep.truncate(0)
