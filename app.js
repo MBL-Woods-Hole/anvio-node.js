b9c6e0144eaab55e4c2311b97283a22055d5ea68
avoorhis <avoorhis@mbl.edu>
2024-06-19 12:33:41 -0400

    ext wait

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ const holdBeforeFileExists = async (filePath, timeout) => {
       return new Promise(resolve => {
         var inter = setInterval(() => {
           nom = nom + 100
-          console.log('nom',nom,'timeout',timeout)
+          //console.log('nom',nom,'timeout',timeout)
           if (nom >= timeout) {
             clearInterval(inter)
             //maybe exists, but my time is up! 
@@ -249,7 +249,7 @@ const holdBeforeFileExists = async (filePath, timeout) => {
             console.log("clear timer, even though there's still plenty of time left")
             resolve(true)
           }
-        }, 100)
+        }, 1000)
       })
   } catch (error) {
     console.log('holdBeforeFileExists err')
